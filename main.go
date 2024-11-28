bf047bb34d1080f840dee0eb789dac7270e11be6
AngelVI <a.v.iliev13@gmail.com>
2024-11-28 16:40:59 +0200

    create all publisher files

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"text/template"
+
+	"github.com/alexflint/go-arg"
 )
 
 type PublisherData struct {
@@ -31,7 +33,51 @@ func createTemplate(name, templateFilename string) (*template.Template, error) {
 	return templ, nil
 }
 
+func createPublisherData(n int) []PublisherData {
+	if n <= 0 {
+		log.Fatalf("number of publishers must be > 0: but got %d", n)
+	}
+	var data []PublisherData
+
+	for i := 1; i <= n; i++ {
+		data = append(data, PublisherData{
+			TopicName: fmt.Sprintf("test%d", i),
+			Name:      fmt.Sprintf("car%d", i),
+			Value:     i,
+		})
+	}
+
+	return data
+}
+
+func createPublisherProcesses(data []PublisherData, tmpl *template.Template) error {
+	tempDir, err := os.MkdirTemp("./", "temp")
+	if err != nil {
+		return fmt.Errorf("failed to create temp dir: %v", err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	for i, d := range data {
+		f, err := os.CreateTemp(tempDir, "pub")
+		if err != nil {
+			return fmt.Errorf("failed to create temp file %d: %v", i, err)
+		}
+
+		err = tmpl.Execute(f, d)
+		if err != nil {
+			return fmt.Errorf("failed to execute template %q: %v", tmpl.Name(), err)
+		}
+	}
+
+	return nil
+}
+
 func main() {
+	var args struct {
+		PubNum int `arg:"-n,--num"     default:"100"   help:"Number of publishers to start"`
+	}
+	arg.MustParse(&args)
+
 	publisherTemplate, err := createTemplate("publisher", "./templates/publisher.py")
 	if err != nil {
 		log.Fatal(err)
@@ -42,17 +88,14 @@ func main() {
 		log.Fatal(err)
 	}
 
-	data := PublisherData{
-		TopicName: "test1",
-		Name:      "name1",
-		Value:     1.0,
-	}
-	err = publisherTemplate.Execute(os.Stdout, data)
+	data := createPublisherData(args.PubNum)
+
+	err = createPublisherProcesses(data, publisherTemplate)
 	if err != nil {
-		log.Fatalf("failed to execute template %q: %v", publisherTemplate.Name(), err)
+		log.Fatal(err)
 	}
 
-	err = vcdlTemplate.Execute(os.Stdout, []PublisherData{data})
+	err = vcdlTemplate.Execute(os.Stdout, data)
 	if err != nil {
 		log.Fatalf("failed to execute template %q: %v", vcdlTemplate.Name(), err)
 	}
